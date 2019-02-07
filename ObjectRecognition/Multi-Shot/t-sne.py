b5b778c06475e0b48461aa3884b3cdfd2b4cef2b
Holstrup <abho@dtu.dk>
2019-02-07 19:11:29 +0100

    * Updated requirements * Refactoring og t-sne
    Signed-off-by: Holstrup <abho@dtu.dk>

--- ObjectRecognition/Multi-Shot/t-sne.py
+++ ObjectRecognition/Multi-Shot/t-sne.py
@@ -5,28 +5,6 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-(train_xs, train_ys), (test_xs, test_ys) = mnist.load_data()
-dim_x = train_xs.shape[1] * train_xs.shape[2]
-dim_y = 10
-
-
-train_xs = train_xs.reshape(train_xs.shape[0], dim_x).astype(np.float32)
-
-scaler = preprocessing.MinMaxScaler().fit(train_xs)
-train_xs = scaler.transform(train_xs)
-print(train_xs.shape)
-print(train_ys.shape)
-
-ridx = np.random.randint(train_xs.shape[0], size=1000)
-np_train_xs = train_xs[ridx, :]
-np_train_ys = train_ys[ridx]
-print(np_train_xs.shape)
-print(np_train_ys.shape)
-
-sns.set_style('darkgrid')
-sns.set_palette('muted')
-sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
-
 
 def draw_scatter(x, n_class, colors):
     sns.palplot(sns.color_palette("hls", n_class))
@@ -41,5 +19,31 @@ def draw_scatter(x, n_class, colors):
     ax.axis('tight')
     plt.show()
 
-tsne_train_xs = TSNE(random_state=42).fit_transform(np_train_xs)
-draw_scatter(tsne_train_xs, dim_y, np_train_ys)
+
+def t_sne_mnist():
+    (train_xs, train_ys), (test_xs, test_ys) = mnist.load_data()
+    dim_x = train_xs.shape[1] * train_xs.shape[2]
+    dim_y = 10
+
+    train_xs = train_xs.reshape(train_xs.shape[0], dim_x).astype(np.float32)
+
+    scaler = preprocessing.MinMaxScaler().fit(train_xs)
+    train_xs = scaler.transform(train_xs)
+    print(train_xs.shape)
+    print(train_ys.shape)
+
+    ridx = np.random.randint(train_xs.shape[0], size=1000)
+    np_train_xs = train_xs[ridx, :]
+    np_train_ys = train_ys[ridx]
+    print(np_train_xs.shape)
+    print(np_train_ys.shape)
+
+    sns.set_style('darkgrid')
+    sns.set_palette('muted')
+    sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
+
+    tsne_train_xs = TSNE(random_state=42).fit_transform(np_train_xs)
+    draw_scatter(tsne_train_xs, dim_y, np_train_ys)
+
+
+t_sne_mnist()
