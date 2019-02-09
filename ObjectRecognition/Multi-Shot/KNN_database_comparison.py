ce6328c642a6250fd99a742cc6b09a05f319f66d
Cristian Botezatu <s164571@student.dtu.dk>
2019-02-09 15:29:37 +0100

    KNN will out the label of the closest neighbour based on its encoding.

--- ObjectRecognition/Multi-Shot/KNN_database_comparison.py
+++ ObjectRecognition/Multi-Shot/KNN_database_comparison.py
@@ -1,13 +1,18 @@
 from sklearn.neighbors import NearestNeighbors
 from database_actions import *
 
-def knn(encodings):
 
+def knn(encodings):
+    """
+    Once being trained, the already existent data will be clustered corresponding to their nature.
+    Thus, by comparing the encodings and finding the 1 nearest neighbour, we will assign the new sample to the proper cluster via its label.
+    """
     samples = get_known_encodings()[0].transpose()
     neigh = NearestNeighbors(n_neighbors=1)
     neigh.fit(samples)
     i = neigh.kneighbors(encodings, return_distance=False)
     label = get_known_encodings()[1][i[0][0]]
     print(label)
+
 
 knn([np.zeros(128)])
