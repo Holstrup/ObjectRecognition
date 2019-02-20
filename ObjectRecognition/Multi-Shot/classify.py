8633daebb26a2b1fb43bedd1f6ef97b71c746cdf
Alexander <abho@dtu.dk>
2019-02-20 19:27:12 +0100

    * Implemented structure for populating database
    Signed-off-by: Holstrup <abho@dtu.dk>

--- ObjectRecognition/Multi-Shot/classify.py
+++ ObjectRecognition/Multi-Shot/classify.py
@@ -1,13 +1,12 @@
 from keras import Model
-
-from keras import Model
-
-from model import mnist_test_set
-from knn import new_knn_function
-import database_actions
+from keras_preprocessing import image
 from keras.models import load_model
 from keras.losses import categorical_crossentropy
 from keras.optimizers import Adam
+import os
+from knn import new_knn_function
+from model import mnist_test_set
+import database_actions
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -20,6 +19,7 @@ def load_model_from_file(filepath):
     global model
     model = load_model(filepath)
     model.summary()
+
 
 def load_imagenet_model(filepath):
     """
@@ -102,18 +102,32 @@ def run_classify(model_name_time):
     print "Correct: " + str(correct)
 
 
-def run_classify_imagenet(model_name_time):
+def run_classify_imagenet():
     """
     Runs a test of the fine-tuned imagenet network
 
     :param model_name_time: model name
     """
     database_actions.reinitialize_table()
-    load_model_from_file("models/" + model_name_time)
-    model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
-    model.summary()
+    TEST_DIR = "Dataset/test/greek/"
+    MODEL_NAME = "resnet50_fully_trained"
+    NUM_CLASSES = 10
 
-    #TODO: Load Data
 
-    #TODO: Test
+    file_list = os.listdir(TEST_DIR)[1:]
+    load_imagenet_model("models/" + MODEL_NAME)
+    imagenet_model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
+    imagenet_model.summary()
+
+
+    for imgFile in file_list:
+        img = image.load_img(TEST_DIR + imgFile, target_size=(224, 224))
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        encoding = imagenet_model.predict(img, batch_size=1)
+        label = imgFile[0]
+        database_actions.add_encoding(encoding, label)
+
+
+run_classify_imagenet()
