c2d107e3b3e58fb6da64cb7f1b5ee8f73e80d5d8
Cristian Botezatu <s164571@student.dtu.dk>
2019-02-26 22:25:03 +0100

    Refactoring

--- ObjectRecognition/Multi-Shot/knn.py
+++ ObjectRecognition/Multi-Shot/knn.py
@@ -2,23 +2,6 @@ from sklearn.neighbors import NearestNeighbors
 from database_actions import get_known_encodings
 import numpy as np
 
-def knn_function(encoding):
-    """
-    Once being trained, the already existent data will be clustered corresponding to their nature.
-    Thus, by comparing the encodings and finding the 1 nearest neighbour, we will assign the new sample to the proper cluster via its label.
-
-    :param encoding: Encoding of an image
-    :return: Predicted label of the image
-    """
-    samples = get_known_encodings()[0].transpose()
-    neigh = NearestNeighbors(n_neighbors=1)
-    neigh.fit(samples)
-    i = neigh.kneighbors(encoding, return_distance=False)
-    label = get_known_encodings()[1][i[0][0]]
-    return label
-
-
-
 def new_knn_function(encodings):
     """
     Function that returns the nearest neighbor to an image encoding
